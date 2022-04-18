b502979ff3421a4ff53926090f58ae1810232a36
techstackmedia <osagienoah04@gmail.com>
2022-04-18 17:04:42 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.dummyText}>Hello World!</Text>
+      <Text style={styles.dummyText}>Another Piece of Text</Text>
+      <Button title="Tap Me!" />
     </View>
   );
 }
 
+// we get auto-completion and validation (warnings and errors) when we use stylesheet object
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  dummyText: {
+    margin: 10,
+    borderWidth: 1,
+    borderColor: "red",
+    borderRadius: 4,
+    padding: 20,
   },
 });
